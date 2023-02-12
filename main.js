7de8330ed20b818b3801917d7dd0f7a237ee5303
filomenoPonce580 <110742301+filomenoPonce580@users.noreply.github.com>
2023-02-12 08:14:16 -0800

    Update main.js
    working. using an if statement

--- main.js
+++ main.js
@@ -1,18 +1,7 @@
-//not operable just yet
-
 function getMiddle(str){
-  //create target
-  let targetIndex = 0;
-  
-  //count the length of string
-  let length = str.length;
-  
-  //string.length/2
-  //if even, grab index (length)/2 & ((length)/2)-1
-  if(length%2 === 0){
-    return str.slice(length/2 - 1, length/2)
+  if (array.length%2 === 1){
+    return array[Math.floor(array.length/2)]
   }else{
-    return str[length/2 - 1]
+    return array[Math.floor(array.length/2)-1] + array[Math.floor(array.length/2)]
   }
-  //if odd, grab index# (length/2)+1
 }
